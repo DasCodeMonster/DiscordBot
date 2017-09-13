b04332e9e0ea8e6ac6d42a73fd81a71f48ae97e2
DasCodeMonster <pl.lkw.01@gmail.com>
2017-09-13 20:21:55 +0200

    added all

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -15,15 +15,18 @@ class Queue extends commando.Command {
     }
     async run(message, args){
         if (this.client.provider.get(message.guild, "queue")) this.queue = await this.client.provider.get(message.guild, "queue");
-        console.log(this.queue);
         if (this.queue.length == 0) {
             message.reply("The queue is empty!");
             return;
         }
         else {
             var messageBuilder = "```";
-            await this.queue.forEach(song => {
-                messageBuilder += song[1] + "\n";
+            await this.queue.some((element, index) => {
+                if (index === 50) return true;
+                else {
+                    messageBuilder += element[1] + "\n";
+                    return false;
+                }
             });
             messageBuilder += "```";
             message.reply(messageBuilder);
