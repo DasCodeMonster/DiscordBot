75cf380d470621b185ad03f08fa85ac5f1e6a772
DasCodeMonster <pl.lkw.01@gmail.com>
2017-10-01 21:43:57 +0200

    added all

--- commands/music/playV2.js
+++ commands/music/playV2.js
@@ -5,19 +5,18 @@ const google = require('googleapis');
 const youtubeV3 = google.youtube({version: "v3", auth: keys.YoutubeAPIKey});
 const Song = require("./Song");
 
-class List extends commando.Command {
+class Play extends commando.Command {
     constructor(client) {
         super(client, {
-            name: "queueadd",
-            aliases: ["qa", "qadd", "add"],
+            name: "play",
             group: "music",
-            memberName: "queueadd",
-            description: "Adds a song to the queue",
+            memberName: "play",
+            description: "plays a song",
             guildOnly: true,
             args: [{
                 key: "link",
                 label: "link",
-                prompt: "Which song would you like to add to the queue? Just give me the link!",
+                prompt: "Which song would you like to play? Just give me the link or search for a song!",
                 type: "string"
             }]
         });
@@ -49,7 +48,7 @@ class List extends commando.Command {
     async validation(message, args) {
         var ID = args.link.split(/([v=&])+/)[2];
         console.log(ID);
-        if(ytdl.validateId(ID)) {
+        if(ID && ytdl.validateId(ID)) {
             this.addSingle(message, args, ID);
         }
         else {
@@ -73,8 +72,9 @@ class List extends commando.Command {
                     //console.log(duration);
                     var song = new Song(item.id, item.snippet.title, item.snippet.channelTitle, item.contentDetails.duration, message.member.id);
                     console.log(song);
-                    this.queue.push(song);
-                    if(!message.guild.voiceConnection.dispatcher) this.play(message);
+                    this.queue.splice(0, 0, song);
+                    if(message.guild.voiceConnection.dispatcher) message.guild.voiceConnection.dispatcher.end("!play");
+                    else this.play(message);
                     console.log(this.queue);
                 });
             }
@@ -90,7 +90,8 @@ class List extends commando.Command {
         }, (err, data) => {
             if (err) {
                 console.log(err);
-                message.reply("this is not a valid link");
+                //message.reply("this is not a valid link");
+                this.search(message, args);
             }
             else {
                 if (!message.guild.voiceConnection){
@@ -172,6 +173,56 @@ class List extends commando.Command {
             }
         });
     }
+    async search(message, args) {
+        youtubeV3.search.list({
+            part: "snippet",
+            type: "video",
+            maxResults: 5,
+            q: args.link
+        }, (err, data) => {
+            if (err) {
+                console.log(err);
+                message.reply("an error occured!");
+            }
+            else {
+                console.log(data);
+                var messageBuilder = "you searched for:" + args.link + "\n```"
+                data.items.forEach((item, index) => {
+                    messageBuilder += `${index+1} ${item.snippet.title}\n`;
+                });
+                messageBuilder += "```"
+                console.log(messageBuilder);
+                this.waitForMessage(message, args, messageBuilder, data)
+            }
+        });
+    }
+    async waitForMessage(message, args, oneLiner, response) {
+        console.log(oneLiner);
+        var commandmsg = await message.reply("type the number of the song to play:\n"+oneLiner+"Respond with ``cancel`` to cancel the command.\n"+
+            "The command will automatically be cancelled in 30 seconds, unless you respond.");
+        const responses = await message.channel.awaitMessages(msg2 => {
+            if (msg2.author.id === message.author.id) {
+                console.log(msg2.id);
+                msg2.delete();
+                return true;
+            }}, {
+            maxMatches: 1,
+            time: 30000,
+            errors: ["time"]
+        });
+        var value;
+        if(responses && responses.size === 1) value = responses.first().content; else return null;
+        if(value.toLowerCase() === 'cancel') {
+            commandmsg.delete();
+            return null;
+        }
+        message
+        commandmsg.delete();
+        console.log(value);
+        console.log(response.items[value-1].id.videoId);
+        await message.member.voiceChannel.join();
+        this.addSingle(message, args, response.items[value-1].id.videoId);
+    }
     async play(message) {
         if (this.queue.length > 0) {
             var vid = this.queue.splice(0, 1)[0];
@@ -211,4 +262,4 @@ class List extends commando.Command {
         }
     }
 }
-module.exports = List;
+module.exports = Play;
